891219ef1ecab4abcc92129827e9754405607bd4
Martin Šimeček <simecek.m@seznam.cz>
2020-01-20 17:47:32 +0100

    update init DB script

--- src/database/script/init.js
+++ src/database/script/init.js
@@ -1,23 +1,120 @@
 const connection = new Mongo();
 const db = connection.getDB("formula1");
+db.dropDatabase();
 
-db.drivers.insertMany([
-  {
-    firstname: "Lewis",
-    lastname: "Hamilton",
-    height: 174,
-    birthday: "07.01.1985",
-    nationality: "BRITISH",
-    points: 413,
-    position: 1
-  },
-  {
-    firstname: "Sebastian",
-    lastname: "Vettel",
-    height: 175,
-    birthday: "03.07.1987",
-    nationality: "GERMAN",
-    points: 240,
-    position: 5
-  }
-]);
+// drivers
+const hamilton = {
+  _id: ObjectId(),
+  firstname: "Lewis",
+  lastname: "Hamilton",
+  height: 1.74,
+  birthday: "07.01.1985",
+  nationality: "BRITISH",
+  points: 413,
+  position: 1
+};
+
+const bottas = {
+  _id: ObjectId(),
+  firstname: "Valtteri",
+  lastname: "Bottas",
+  height: 1.73,
+  birthday: "28.08.1989",
+  nationality: "FINNISH",
+  points: 326,
+  position: 2
+};
+
+const vettel = {
+  _id: ObjectId(),
+  firstname: "Sebastian",
+  lastname: "Vettel",
+  height: 1.75,
+  birthday: "03.07.1987",
+  nationality: "GERMAN",
+  points: 240,
+  position: 5
+};
+
+const leclerc = {
+  _id: ObjectId(),
+  firstname: "Charles",
+  lastname: "Leclerc",
+  height: 1.8,
+  birthday: "16.10.1997",
+  nationality: "MONACO",
+  points: 264,
+  position: 4
+};
+
+const verstappen = {
+  _id: ObjectId(),
+  firstname: "Max",
+  lastname: "Verstappen",
+  height: 1.8,
+  birthday: "30.09.1997",
+  nationality: "DUTCH",
+  points: 278,
+  position: 3
+};
+
+const albon = {
+  _id: ObjectId(),
+  firstname: "Alexander",
+  lastname: "Albon",
+  height: 1.86,
+  birthday: "23.03.1996",
+  nationality: "THAI",
+  points: 92,
+  position: 8
+};
+
+// teams
+const mercedes = {
+  _id: ObjectId(),
+  name: "Mercedes",
+  principal: "Toto Wolff",
+  drivers: [hamilton._id, bottas._id],
+  position: 1,
+  points: 739,
+  color: "#00D2BE"
+  //TODO: complete data
+  // residence: Residence!
+  // car: Car!
+};
+
+const ferrari = {
+  _id: ObjectId(),
+  name: "Ferrari",
+  principal: "Mattia Binotto",
+  drivers: [vettel._id, leclerc._id],
+  position: 2,
+  points: 504,
+  color: "#DC0000"
+  //TODO: complete data
+  // residence: Residence!
+  // car: Car!
+};
+
+const redbull = {
+  _id: ObjectId(),
+  name: "Redbull",
+  principal: "Christian Horner",
+  drivers: [verstappen._id, albon._id],
+  position: 3,
+  points: 417,
+  color: "#1E41FF"
+  //TODO: complete data
+  // residence: Residence!
+  // car: Car!
+};
+
+hamilton.team = mercedes._id;
+bottas.team = mercedes._id;
+vettel.team = ferrari._id;
+leclerc.team = ferrari._id;
+verstappen.team = redbull._id;
+albon.team = redbull._id;
+
+db.drivers.insertMany([hamilton, bottas, vettel, leclerc, verstappen, albon]);
+db.teams.insertMany([mercedes, ferrari, redbull]);
