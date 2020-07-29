5d509379dce7218b23975221e6e0c95faee45ae2
Martin Šimeček <simecek.m@seznam.cz>
2020-07-29 10:41:45 +0200

    add Bahrain 2019 qualifying

--- src/database/script/documents/grandsPrix/2019/qualifying.js
+++ src/database/script/documents/grandsPrix/2019/qualifying.js
@@ -1,2 +1,5 @@
 // Australia
 load("./documents/grandsPrix/2019/qualifying/australia.js");
+
+// Bahrain
+load("./documents/grandsPrix/2019/qualifying/bahrain.js");
---
+++ src/database/script/documents/grandsPrix/2019/qualifying/bahrain.js
@@ -0,0 +1,159 @@
+const bahrainGrandPrix2019LeclercQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:28.495",
+  q2Time: "1:28.046",
+  q3Time: "1:27.866",
+  position: 1
+};
+
+const bahrainGrandPrix2019VettelQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:28.733",
+  q2Time: "1:28.356",
+  q3Time: "1:28.160",
+  position: 2
+};
+
+const bahrainGrandPrix2019HamiltonQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.262",
+  q2Time: "1:28.578",
+  q3Time: "1:28.190",
+  position: 3
+};
+
+const bahrainGrandPrix2019BottasQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.498",
+  q2Time: "1:28.830",
+  q3Time: "1:28.256",
+  position: 4
+};
+
+const bahrainGrandPrix2019VerstappenQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.579",
+  q2Time: "1:29.109",
+  q3Time: "1:28.752",
+  position: 5
+};
+
+const bahrainGrandPrix2019MagnussenQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.532",
+  q2Time: "1:29.017",
+  q3Time: "1:28.757",
+  position: 6
+};
+
+const bahrainGrandPrix2019SainzQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.528",
+  q2Time: "1:29.055",
+  q3Time: "1:28.813",
+  position: 7
+};
+
+const bahrainGrandPrix2019GrosjeanQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.688",
+  q2Time: "1:29.249",
+  q3Time: "1:29.015",
+  position: 8
+};
+
+const bahrainGrandPrix2019RaikkonenQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.959",
+  q2Time: "1:29.471",
+  q3Time: "1:29.022",
+  position: 9
+};
+
+const bahrainGrandPrix2019NorrisQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.381",
+  q2Time: "1:29.258",
+  q3Time: "1:29.043",
+  position: 10
+};
+
+const bahrainGrandPrix2019RicciardoQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.859",
+  q2Time: "1:29.488",
+  q3Time: null,
+  position: 11
+};
+
+const bahrainGrandPrix2019AlbonQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.514",
+  q2Time: "1:29.513",
+  q3Time: null,
+  position: 12
+};
+
+const bahrainGrandPrix2019GaslyQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.900",
+  q2Time: "1:29.526",
+  q3Time: null,
+  position: 13
+};
+
+const bahrainGrandPrix2019PerezQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.893",
+  q2Time: "1:29.756",
+  q3Time: null,
+  position: 14
+};
+
+const bahrainGrandPrix2019KvyatQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:29.876",
+  q2Time: "1:29.854",
+  q3Time: null,
+  position: 15
+};
+
+const bahrainGrandPrix2019GiovinazziQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.026",
+  q2Time: null,
+  q3Time: null,
+  position: 16
+};
+
+const bahrainGrandPrix2019HulkenbergQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.034",
+  q2Time: null,
+  q3Time: null,
+  position: 17
+};
+
+const bahrainGrandPrix2019StrollQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.217",
+  q2Time: null,
+  q3Time: null,
+  position: 18
+};
+
+const bahrainGrandPrix2019RusellQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.759",
+  q2Time: null,
+  q3Time: null,
+  position: 19
+};
+
+const bahrainGrandPrix2019BubicaQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.799",
+  q2Time: null,
+  q3Time: null,
+  position: 20
+};
