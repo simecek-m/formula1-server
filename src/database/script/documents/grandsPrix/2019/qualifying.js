5644ff98cb1efc17a0791a40ea450207a98b1882
Martin Šimeček <simecek.m@seznam.cz>
2020-07-29 12:46:18 +0200

    add China 2019 qualifying

--- src/database/script/documents/grandsPrix/2019/qualifying.js
+++ src/database/script/documents/grandsPrix/2019/qualifying.js
@@ -3,3 +3,6 @@ load("./documents/grandsPrix/2019/qualifying/australia.js");
 
 // Bahrain
 load("./documents/grandsPrix/2019/qualifying/bahrain.js");
+
+// China
+load("./documents/grandsPrix/2019/qualifying/china.js");
---
+++ src/database/script/documents/grandsPrix/2019/qualifying/china.js
@@ -0,0 +1,159 @@
+const chinaGrandPrix2019BottasQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:32.658",
+  q2Time: "1:31.728",
+  q3Time: "1:31.547",
+  position: 1
+};
+
+const chinaGrandPrix2019HamiltonQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:33.115",
+  q2Time: "1:31.637",
+  q3Time: "1:31.570",
+  position: 2
+};
+
+const chinaGrandPrix2019VettelQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:33.557",
+  q2Time: "1:32.232",
+  q3Time: "1:31.848",
+  position: 3
+};
+
+const chinaGrandPrix2019LeclercQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:32.712",
+  q2Time: "1:32.324",
+  q3Time: "1:31.865",
+  position: 4
+};
+
+const chinaGrandPrix2019VerstappenQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:33.274",
+  q2Time: "1:32.369",
+  q3Time: "1:32.089",
+  position: 5
+};
+
+const chinaGrandPrix2019GaslyQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:33.863",
+  q2Time: "1:32.948",
+  q3Time: "1:32.930",
+  position: 6
+};
+
+const chinaGrandPrix2019RicciardoQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:33.709",
+  q2Time: "1:33.214",
+  q3Time: "1:32.958",
+  position: 7
+};
+
+const chinaGrandPrix2019HulkenbergQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:33.644",
+  q2Time: "1:32.968",
+  q3Time: "1:32.962",
+  position: 8
+};
+
+const chinaGrandPrix2019MagnussenQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:34.036",
+  q2Time: "1:33.150",
+  q3Time: null,
+  position: 9
+};
+
+const chinaGrandPrix2019GrosjeanQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:33.752",
+  q2Time: "1:33.156",
+  q3Time: null,
+  position: 10
+};
+
+const chinaGrandPrix2019KvyatQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:33.783",
+  q2Time: "1:33.236",
+  q3Time: null,
+  position: 11
+};
+
+const chinaGrandPrix2019PerezQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:34.026",
+  q2Time: "1:33.299",
+  q3Time: null,
+  position: 12
+};
+
+const chinaGrandPrix2019RaikkonenQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:34.125",
+  q2Time: "1:33.419",
+  q3Time: null,
+  position: 13
+};
+
+const chinaGrandPrix2019SainzQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:33.686",
+  q2Time: "1:33.523",
+  q3Time: null,
+  position: 14
+};
+
+const chinaGrandPrix2019NorrisQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:34.148",
+  q2Time: "1:33.967",
+  q3Time: null,
+  position: 15
+};
+
+const chinaGrandPrix2019StrollQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:34.292",
+  q2Time: null,
+  q3Time: null,
+  position: 16
+};
+
+const chinaGrandPrix2019RusellQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:35.253",
+  q2Time: null,
+  q3Time: null,
+  position: 17
+};
+
+const chinaGrandPrix2019BubicaQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:35.281",
+  q2Time: null,
+  q3Time: null,
+  position: 18
+};
+
+const chinaGrandPrix2019GiovinazziQualifying = {
+  _id: ObjectId(),
+  q1Time: null,
+  q2Time: null,
+  q3Time: null,
+  position: 19
+};
+
+const chinaGrandPrix2019AlbonQualifying = {
+  _id: ObjectId(),
+  q1Time: null,
+  q2Time: null,
+  q3Time: null,
+  position: 20
+};
