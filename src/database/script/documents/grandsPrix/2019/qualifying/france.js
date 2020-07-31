78272d86f34a5f83b564b29a701307979f175c28
Martin Šimeček <simecek.m@seznam.cz>
2020-07-31 13:18:07 +0200

    add France 2019 qualifying

---
+++ src/database/script/documents/grandsPrix/2019/qualifying/france.js
@@ -0,0 +1,179 @@
+const franceGrandPrix2019HamiltonQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.609",
+  q2Time: "1:29.520",
+  q3Time: "1:28.319",
+  note: null,
+  position: 1
+};
+
+const franceGrandPrix2019BottasQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.550",
+  q2Time: "1:29.437",
+  q3Time: "1:28.605",
+  note: null,
+  position: 2
+};
+
+const franceGrandPrix2019LeclercQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.647",
+  q2Time: "1:29.699",
+  q3Time: "1:28.965",
+  note: null,
+  position: 3
+};
+
+const franceGrandPrix2019VerstappenQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.327",
+  q2Time: "1:30.099",
+  q3Time: "1:29.409",
+  note: null,
+  position: 4
+};
+
+const franceGrandPrix2019NorrisQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.989",
+  q2Time: "1:30.019",
+  q3Time: "1:29.418",
+  note: null,
+  position: 5
+};
+
+const franceGrandPrix2019SainzQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.073",
+  q2Time: "1:30.319",
+  q3Time: "1:29.522",
+  note: null,
+  position: 6
+};
+
+const franceGrandPrix2019VettelQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.075",
+  q2Time: "1:29.506",
+  q3Time: "1:29.799",
+  note: null,
+  position: 7
+};
+
+const franceGrandPrix2019RicciardoQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.954",
+  q2Time: "1:30.369",
+  q3Time: "1:29.918",
+  note: null,
+  position: 8
+};
+
+const franceGrandPrix2019GaslyQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.152",
+  q2Time: "1:30.421",
+  q3Time: "1:30.184",
+  note: null,
+  position: 9
+};
+
+const franceGrandPrix2019GiovinazziQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.180",
+  q2Time: "1:30.408",
+  q3Time: "1:33.420",
+  note: null,
+  position: 10
+};
+
+const franceGrandPrix2019AlbonQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.445",
+  q2Time: "1:30.461",
+  q3Time: null,
+  note: null,
+  position: 11
+};
+
+const franceGrandPrix2019RaikkonenQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.972",
+  q2Time: "1:30.533",
+  q3Time: null,
+  note: null,
+  position: 12
+};
+
+const franceGrandPrix2019HulkenbergQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.865",
+  q2Time: "1:30.544",
+  q3Time: null,
+  note: null,
+  position: 13
+};
+
+const franceGrandPrix2019PerezQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:30.964",
+  q2Time: "1:30.738",
+  q3Time: null,
+  note: null,
+  position: 14
+};
+
+const franceGrandPrix2019MagnussenQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.166",
+  q2Time: "1:31.440",
+  q3Time: null,
+  note: null,
+  position: 15
+};
+
+const franceGrandPrix2019KvyatQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.564",
+  q2Time: null,
+  q3Time: null,
+  note: "Start from back of grid due to multiple power unit element changes.",
+  position: 16
+};
+
+const franceGrandPrix2019GrosjeanQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.626",
+  q2Time: null,
+  q3Time: null,
+  note: null,
+  position: 17
+};
+
+const franceGrandPrix2019StrollQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:31.726",
+  q2Time: null,
+  q3Time: null,
+  note: null,
+  position: 18
+};
+
+const franceGrandPrix2019RussellQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:32.789",
+  q2Time: null,
+  q3Time: null,
+  note: "Start from back of grid due to multiple power unit element changes.",
+  position: 19
+};
+
+const franceGrandPrix2019KubicaQualifying = {
+  _id: ObjectId(),
+  q1Time: "1:33.205",
+  q2Time: null,
+  q3Time: null,
+  note: null,
+  position: 20
+};
