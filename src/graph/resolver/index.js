76f4c1ac6da1f905dbeea8850eacbe641e48230c
Martin Šimeček <simecek.m@seznam.cz>
2020-08-11 08:41:51 +0200

    add Season, Race and Qualifying resolvers

--- src/graph/resolver/index.js
+++ src/graph/resolver/index.js
@@ -13,20 +13,20 @@ const Circuit = require("@database/model/circuit/Circuit");
 const Race = require("@database/model/race/Race");
 const Season = require("@database/model/season/Season");
 const Factory = require("@database/model/location/Factory");
+const Qualifying = require("@database/model/race/Qualifying");
 
 const resolvers = {
   Query: {
-    drivers: () => Driver.find(),
-    driver: (_, { id }) => Driver.findById(id),
+    drivers: ( _, args) => Driver.find(args),
     driverSeasons: () => DriverSeason.find(),
-    teams: () => Team.find(),
+    teams: (_, args) => Team.find(args),
     teamSeasons: () => TeamSeason.find(),
     cars: () => Car.find(),
-    countries: () => Country.find(),
+    countries: (_, args) => Country.find(args),
     factories: () => Factory.find(),
-    circuits: () => Circuit.find(),
-    seasons: () => Season.find(),
-    races: () => Race.find(),
+    circuits: (_, args) => Circuit.find(args),
+    seasons: (_, args) => Season.find(args),
+    races: (_, args) => Race.find(args),
   },
   Date: new GraphQLScalarType({
     name: "Date",
@@ -110,7 +110,34 @@ const resolvers = {
     },
     country: ({ country }) => Country.findById(country),
     races: ({ races }) => Race.find({ _id: { $in: races }})
+  },
+  Season: {
+    driverSeasons: ({ drivers }) => DriverSeason.find({ _id: { $in: drivers }}),
+    teamSeasons: ({ teams }) => TeamSeason.find({ _id: { $in: teams }}),
+    races: ({ races }) => Race.find({ _id: { $in: races }}),
+    cars: ({ cars }) => Car.find({ _id: { $in: cars }})
+  },
+  Race: {
+    season: ({ season }) => Season.findById(season),
+    circuit: ({ circuit }) => Circuit.findById(circuit),
+    qualifying: ({ qualifying }) => Qualifying.find({ _id: { $in: qualifying }})
+    // TODO: fastestLaps: ({ fastestLaps }) => FastestLap.find({ _id: { $in: fastestLaps }})
+    // TODO: results: ({ results }) => Result.find({ _id: { $in: results }})
+  },
+  Qualifying: {
+    driver: ({ driver }) => Driver.findById(driver),
+    team: ({ team }) => Team.findById(team)
   }
+  /* TODO: fastestLap and result resolvers
+  FastestLap: {
+    driver: ({ driver }) => Driver.findById(driver),
+    team: ({ team }) => Team.findById(team)
+  }
+  Result: {
+    driver: ({ driver }) => Driver.findById(driver),
+    team: ({ team }) => Team.findById(team)
+  }
+  */
 };
 
 module.exports = resolvers;
