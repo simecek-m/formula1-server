24dc65ceceabb9fdcd87967b0618bfb052cc7114
Martin Šimeček <simecek.m@seznam.cz>
2020-08-11 08:40:53 +0200

    add Qualifying schema

---
+++ src/database/model/race/Qualifying.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.ObjectId;
+
+const qualifyingSchema = new mongoose.Schema({
+  _id: {
+    type: ObjectId,
+    required: true
+  },
+  driver: {
+    type: ObjectId,
+    required: true
+  },
+  q1Time: {
+    type: String,
+    required: true
+  },
+  q2Time: {
+    type: String,
+    required: true
+  },
+  q3Time: {
+    type: String,
+    required: true
+  },
+  position: {
+    type: Number,
+    required: true
+  },
+  note: String,
+  team: {
+    type: ObjectId,
+    required: true
+  }
+});
+
+const Qualifying = mongoose.model("qualifying", qualifyingSchema, "qualifying");
+
+module.exports = Qualifying;
