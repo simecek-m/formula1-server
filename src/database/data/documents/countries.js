8b1123f0a6e6d28482f30ed5a96c3afc54f59b11
Martin Šimeček <simecek.m@seznam.cz>
2020-08-27 22:51:34 +0200

    rename flag asset folder

--- src/database/data/documents/countries.js
+++ src/database/data/documents/countries.js
@@ -1,167 +1,167 @@
 const britain = {
   _id: ObjectId(),
   name: "Great Britain",
-  flag: "/static/assets/flag/britain.svg"
+  flag: "/static/assets/flags/britain.svg"
 };
 
 const finland = {
   _id: ObjectId(),
   name: "Finland",
-  flag: "/static/assets/flag/finland.svg"
+  flag: "/static/assets/flags/finland.svg"
 };
 
 const germany = {
   _id: ObjectId(),
   name: "Germany",
-  flag: "/static/assets/flag/germany.svg"
+  flag: "/static/assets/flags/germany.svg"
 };
 
 const monaco = {
   _id: ObjectId(),
   name: "Monaco",
-  flag: "/static/assets/flag/monaco.svg"
+  flag: "/static/assets/flags/monaco.svg"
 };
 
 const netherlands = {
   _id: ObjectId(),
   name: "Netherlands",
-  flag: "/static/assets/flag/netherlands.svg"
+  flag: "/static/assets/flags/netherlands.svg"
 };
 
 const thailand = {
   _id: ObjectId(),
   name: "Thailand",
-  flag: "/static/assets/flag/thailand.svg"
+  flag: "/static/assets/flags/thailand.svg"
 };
 
 const italy = {
   _id: ObjectId(),
   name: "Italy",
-  flag: "/static/assets/flag/italy.svg"
+  flag: "/static/assets/flags/italy.svg"
 };
 
 const spain = {
   _id: ObjectId(),
   name: "Spain",
-  flag: "/static/assets/flag/spain.svg"
+  flag: "/static/assets/flags/spain.svg"
 };
 
 const australia = {
   _id: ObjectId(),
   name: "Australia",
-  flag: "/static/assets/flag/australia.svg"
+  flag: "/static/assets/flags/australia.svg"
 };
 
 const france = {
   _id: ObjectId(),
   name: "France",
-  flag: "/static/assets/flag/france.svg"
+  flag: "/static/assets/flags/france.svg"
 };
 
 const russia = {
   _id: ObjectId(),
   name: "Russia",
-  flag: "/static/assets/flag/russia.svg"
+  flag: "/static/assets/flags/russia.svg"
 };
 
 const bahrain = {
   _id: ObjectId(),
   name: "Bahrain",
-  flag: "/static/assets/flag/bahrain.svg"
+  flag: "/static/assets/flags/bahrain.svg"
 };
 
 const china = {
   _id: ObjectId(),
   name: "China",
-  flag: "/static/assets/flag/china.svg"
+  flag: "/static/assets/flags/china.svg"
 };
 
 const belgium = {
   _id: ObjectId(),
   name: "Belgium",
-  flag: "/static/assets/flag/belgium.svg"
+  flag: "/static/assets/flags/belgium.svg"
 };
 
 const poland = {
   _id: ObjectId(),
   name: "Poland",
-  flag: "/static/assets/flag/poland.svg"
+  flag: "/static/assets/flags/poland.svg"
 };
 
 const hungary = {
   _id: ObjectId(),
   name: "Hungary",
-  flag: "/static/assets/flag/hungary.svg"
+  flag: "/static/assets/flags/hungary.svg"
 };
 
 const austria = {
   _id: ObjectId(),
   name: "Austria",
-  flag: "/static/assets/flag/austria.svg"
+  flag: "/static/assets/flags/austria.svg"
 };
 
 const canada = {
   _id: ObjectId(),
   name: "Canada",
-  flag: "/static/assets/flag/canada.svg"
+  flag: "/static/assets/flags/canada.svg"
 };
 
 const azerbaijan = {
   _id: ObjectId(),
   name: "Azerbaijan",
-  flag: "/static/assets/flag/azerbaijan.svg"
+  flag: "/static/assets/flags/azerbaijan.svg"
 };
 
 const usa = {
   _id: ObjectId(),
   name: "USA",
-  flag: "/static/assets/flag/usa.svg"
+  flag: "/static/assets/flags/usa.svg"
 };
 
 const brazil = {
   _id: ObjectId(),
   name: "Brazil",
-  flag: "/static/assets/flag/brazil.svg"
+  flag: "/static/assets/flags/brazil.svg"
 };
 
 const japan = {
   _id: ObjectId(),
   name: "Japan",
-  flag: "/static/assets/flag/japan.svg"
+  flag: "/static/assets/flags/japan.svg"
 };
 
 const singapore = {
   _id: ObjectId(),
   name: "Singapore",
-  flag: "/static/assets/flag/singapore.svg"
+  flag: "/static/assets/flags/singapore.svg"
 };
 
 const mexico = {
   _id: ObjectId(),
   name: "Mexico",
-  flag: "/static/assets/flag/mexico.svg"
+  flag: "/static/assets/flags/mexico.svg"
 };
 
 const emirates = {
   _id: ObjectId(),
   name: "United Arab Emirates",
-  flag: "/static/assets/flag/emirates.svg"
+  flag: "/static/assets/flags/emirates.svg"
 };
 
 const vietnam = {
   _id: ObjectId(),
   name: "Vietnam",
-  flag: "/static/assets/flag/vietnam.svg"
+  flag: "/static/assets/flags/vietnam.svg"
 };
 
 const denmark = {
   _id: ObjectId(),
   name: "Denmark",
-  flag: "/static/assets/flag/denmark.svg"
+  flag: "/static/assets/flags/denmark.svg"
 };
 
 const switzerland = {
   _id: ObjectId(),
   name: "Switzerland",
-  flag: "/static/assets/flag/switzerland.svg"
+  flag: "/static/assets/flags/switzerland.svg"
 };
